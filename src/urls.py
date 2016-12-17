c304481fec3907ddc966a0d877dcf76ec3119105
Amritpal Singh <amrit3701@gmail.com>
2016-12-17 15:28:13 +0530

    update

--- src/urls.py
+++ src/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     #directs to index veiw
-    url(r'^index/$', views.index, name='index'),
+    url(r'$', views.index, name='index'),
     url(r'^specs/$', views.specs, name='specs'),
     url(r'^download/$', views.download, name='download')
 ]
